bfc7ab90508ba4c5324b4a3773a9f55a276e0e58
semlhr <alvaneigr1@gmail.com>
2022-03-25 20:08:36 -0400

    update title

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,38 +7,60 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "NeiTheAdventurerTurtle";
+const description = "This is the only turtle with the foresight necessary to save our world, Turtleland. If you are faster than our hero, help him spread his good will by having your unique image of him travel to all those other countries, demonstrating his concern for our nature, culture, and anything else you are wise enough to perceive with your own beautiful eyes.This is the only turtle with the foresight necessary to save our world, Turtleland. If you are faster than our hero, help him spread his good will by having your unique image of him travel to all those other countries, demonstrating his concern for our nature, culture, and anything else you are wise enough to perceive with your own beautiful eyes.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10000,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Sky" },
+      { name: "Foreground" },
+      { name: "Pure Backgrouns" },
+      { name: "Ground" },
+      { name: "Right Trees" },
+      { name: "Left Trees" },
+      { name: "Shadow" },
+      { name: "Right Front Leg" },
+      { name: "Left Front Leg" },
+      { name: "Left Back Leg" },
+      { name: "Right Back Leg" },
+      { name: "Complete Hull" },
+      { name: "T1" },
+      { name: "T2" },
+      { name: "T3" },
+      { name: "T4" },
+      { name: "T5" },
+      { name: "T6" },
+      { name: "T7" },
+      { name: "T8" },
+      { name: "Tail" },
+      { name: "Lower Back" },
+      { name: "Head" },
+      { name: "Nails" },
+      { name: "Eyes" },
+      { name: "Eyes Emotion" },
+      { name: "Mouth" },
+      { name: "Hats" },
+      { name: "Garden" },
     ],
   },
 ];
 
-const shuffleLayerConfigurations = true;
+const shuffleLayerConfigurations = false;
 
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1000,
+  height: 1000,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://www.instagram.com/arochagouvea", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,11 +71,11 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'NeiTheAdventurerTurtle';
+const CONTRACT_SYMBOL = 'NAT';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0x9F221d9Ca06cB8e8F1B88689ecfe462579fFA2D8';
+const TREASURY_ADDRESS = '0x9F221d9Ca06cB8e8F1B88689ecfe462579fFA2D8';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
@@ -64,7 +86,7 @@ const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x9F221d9Ca06cB8e8F1B88689ecfe462579fFA2D8"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -73,7 +95,7 @@ const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
