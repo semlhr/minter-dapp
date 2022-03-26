86567315bf4eb586db6cb51b93fd8a2a880fb504
semlhr <alvaneigr1@gmail.com>
2022-03-26 10:46:28 -0400

    up

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 10000,
+    growEditionSizeTo: 5000,
     layersOrder: [
       { name: "Sky" },
       { name: "Foreground" },
